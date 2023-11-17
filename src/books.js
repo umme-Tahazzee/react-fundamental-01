aef95ce3f2097f799e327e86ae620e591293faa3
= <tahazzeekaria1@gmail.com>
2023-11-17 11:09:03 +0600

    umme-tahazzee

---
+++ src/books.js
@@ -0,0 +1,28 @@
+import img1 from './images/book1.jpg';
+
+
+export const books = [
+    {
+      img: img1,
+      author: "Jordan Moore",
+      title: "Interesting Facts For Curious Minds",
+      id:1
+    },
+    {
+      img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+      author: "James Clear",
+      title: "Atomic Habits",
+      id:2
+    },
+    {
+        img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+        author: "James Clear",
+        title: "Atomic Habits",
+        id:2
+      },
+  ];
+
+
+   
+  
+  
